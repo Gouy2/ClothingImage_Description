64971ba0a0964471ef1b2e1d27ebc0a607fba5da
Gouy2 <2677798075@qq.com>
2023-12-19 23:58:40 +0800

    +dataloader

---
+++ CNN-GRU/dataloader.py
@@ -0,0 +1,91 @@
+from argparse import Namespace 
+import numpy as np
+import os
+import json
+from PIL import Image
+import torch
+import torch.nn as nn
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.utils.data import Dataset
+import torchvision
+import torchvision.transforms as transforms
+
+class ImageTextDataset(Dataset):
+    """
+    PyTorch数据类，用于PyTorch DataLoader来按批次产生数据
+    """
+
+    def __init__(self, dataset_path, vocab_path, split, captions_per_image=5, max_len=30, transform=None):
+        """
+        参数：
+            dataset_path：json格式数据文件路径
+            vocab_path：json格式词典文件路径
+            split：train、val、test
+            captions_per_image：每张图片对应的文本描述数
+            max_len：文本描述包含的最大单词数
+            transform: 图像预处理方法
+        """
+        self.split = split
+        assert self.split in {'train','test'}
+        self.cpi = captions_per_image
+        self.max_len = max_len
+
+        # 载入数据集
+        with open(dataset_path, 'r') as f:
+            self.data = json.load(f)
+        # 载入词典
+        with open(vocab_path, 'r') as f:
+            self.vocab = json.load(f)
+
+        # PyTorch图像预处理流程
+        self.transform = transform
+
+        # Total number of datapoints
+        self.dataset_size = len(self.data['CAPTIONS'])
+
+    def __getitem__(self, i):
+        # 第i个文本描述对应第(i // captions_per_image)张图片
+        img = Image.open(self.data['IMAGES'][i // self.cpi]).convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+
+        caplen = len(self.data['CAPTIONS'][i])
+        caption = torch.LongTensor(self.data['CAPTIONS'][i]+ [self.vocab['<pad>']] * (self.max_len + 2 - caplen)) 
+        
+        return img, caption, caplen
+        
+
+    def __len__(self):
+        return self.dataset_size
+    
+def mktrainval(data_dir, vocab_path, batch_size, workers=4):
+    train_tx = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    val_tx = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    
+    train_set = ImageTextDataset(os.path.join(data_dir, 'train_data.json'), 
+                                 vocab_path, 'train',  transform=train_tx)
+    valid_set = ImageTextDataset(os.path.join(data_dir, 'val_data.json'), 
+                                 vocab_path, 'val', transform=val_tx)
+    test_set = ImageTextDataset(os.path.join(data_dir, 'test_data.json'), 
+                                 vocab_path, 'test', transform=val_tx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, num_workers=workers, pin_memory=True)
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_set, batch_size=batch_size, shuffle=False, num_workers=workers, pin_memory=True, drop_last=False)
+    
+    test_loader = torch.utils.data.DataLoader(
+        test_set, batch_size=batch_size, shuffle=False, num_workers=workers, pin_memory=True, drop_last=False)
+
+    return train_loader, valid_loader, test_loader   
